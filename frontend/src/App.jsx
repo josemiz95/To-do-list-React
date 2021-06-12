9e47892de4e6f2734b2be46c8ab62afef80dbc95
Josemi <josemi@dinan.es>
2021-06-12 13:23:20 +0200

    Adding new tasks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,11 +17,17 @@ export default function App() {
     return task.pending === showingPending;
   });
 
+  const addTask = (task) => { // Funtion to add task
+    setTasks((prevTasks)=>{
+      const newTask = {id:uuid(),description:task,pending:true}
+      return [...prevTasks, newTask];
+    })
+  }
 
   return (
     <div className="container">
       <h1 className="mb-3">To Do List</h1>
-      <TaskForm />
+      <TaskForm addTask={addTask}/>
       <button className="btn btn-primary mb-3" onClick={toggleTaskList}>{showingPending?'Toggle to completed task':'Toggle to Pending task'}</button>
       <List tasks={list}/>
     </div>
--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
-export default function TaskForm() {
+export default function TaskForm({addTask}) {
+    const newTask = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const task = newTask.current.value;
+        if(task.trim !== ''){
+            addTask(task);
+            newTask.current.value = null;
+        }
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Task description"/>
+                    <input ref={newTask} type="text" className="form-control" placeholder="Task description"/>
                     <button className="btn btn-success" type="submit" id="button-addon2">Add Task</button>
                 </div>
             </form>
