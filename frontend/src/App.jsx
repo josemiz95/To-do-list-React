1ec0ed8951a229af70e8eac8a0f3f0ab04ee8b6c
Josemi <josemi@dinan.es>
2021-06-12 12:52:18 +0200

    Button for toggle the list of task

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 // Components
 import List from "./components/List";
 
 
 export default function App() {
+  const [showingPending, setShowingPending] = useState(true); // True show pending task, False show completed task
+
+  const toggleTaskList = () => {
+    setShowingPending(!showingPending); // Action for toggle list
+  }
+
   return (
-    <>
+    <div className="container">
+      <h1 className="mb-3">To Do List</h1>
+      <button className="btn btn-primary mb-3" onClick={toggleTaskList}>{showingPending?'Toggle to completed task':'Toggle to Pending task'}</button>
       <List />
-    </>
+    </div>
   );
 }
