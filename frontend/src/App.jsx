d146801f8dbb00d2fee2f2b5a99200931490cd6e
Josemi <josemi@dinan.es>
2021-06-12 13:05:57 +0200

    Toggle list between pending and done task

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,24 @@ import List from "./components/List";
 export default function App() {
   const [showingPending, setShowingPending] = useState(true); // True show pending task, False show completed task
   const [tasks, setTasks] = useState([
-    {id:uuid(), description:'Something to do', done:false}
+    {id:uuid(), description:'Something to do', pending:true},
+    {id:uuid(), description:'Something to done', pending:false},
   ]);
 
   const toggleTaskList = () => {
     setShowingPending(!showingPending); // Action for toggle list
   }
 
+  const list = tasks.filter((task)=>{ // List of showing tasks
+    return task.pending === showingPending;
+  });
+
+
   return (
     <div className="container">
       <h1 className="mb-3">To Do List</h1>
       <button className="btn btn-primary mb-3" onClick={toggleTaskList}>{showingPending?'Toggle to completed task':'Toggle to Pending task'}</button>
-      <List tasks={tasks}/>
+      <List tasks={list}/>
     </div>
   );
 }
