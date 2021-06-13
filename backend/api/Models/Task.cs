771ae7f4d0de5e1024d598f22b49e3faefa9a752
Josemi <josemi@dinan.es>
2021-06-13 18:40:21 +0200

    Task model

---
+++ backend/api/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Task
+    {
+        public int id { get; set; }
+        [StringLength(150)]
+        public string description { get; set; }
+        public bool pending { get; set; }
+    }
+}
