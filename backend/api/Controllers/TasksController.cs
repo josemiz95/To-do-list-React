c2bfdd3f82e1bf9f0d5d5dd38444f24d75098de4
Josemi <josemi@dinan.es>
2021-06-13 18:44:58 +0200

    Task controller

---
+++ backend/api/Controllers/TasksController.cs
@@ -0,0 +1,22 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")] // Route for the Task api
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ApplicationDbContext context; // DB contex for manipulations
+
+        public TasksController(ApplicationDbContext context)
+        {
+            this.context = context; // Setting up the context
+        }
+    }
+}
