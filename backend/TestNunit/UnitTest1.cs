ea38e3e7cd1ee61fdb645bafbcea1cc7b25d5fc1
Josemi <josemi@dinan.es>
2021-07-10 12:19:32 +0200

    Testing proyect

---
+++ backend/TestNunit/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using api.Models;
+
+namespace TestNunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Task task = new Task()
+            {
+                id = 1,
+                description = "Descripcion de la tarea",
+                pending = true
+            };
+
+            Assert.AreEqual(true, task.pending);
+        }
+    }
+}
