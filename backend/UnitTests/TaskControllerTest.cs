816e0b49b0f132842eb0d0727d0624574b108b24
Josemi Ruiz <jmruiz195@gmail.com>
2022-02-03 20:08:27 +0100

    Get method tests

--- backend/UnitTests/TaskControllerTest.cs
+++ backend/UnitTests/TaskControllerTest.cs
@@ -1,30 +1,32 @@
 ﻿namespace UnitTests
 {
-    using Repository.Contracts;
-    using API.Controllers;
+    
+    using System.Collections.Generic;
     using Microsoft.AspNetCore.Mvc;
     using AutoMapper;
     using API.Mappings;
     using Xunit;
     using AutoFixture;
-    using AutoFixture.AutoNSubstitute;
-    using AutoFixture.AutoMoq;
+    using Moq;
+    using Repository.Contracts;
     using Repository.Models;
+    using API.ViewModels;
+    using API.Controllers;
 
     public class TaskControllerTest
     {
         private readonly IFixture _fixture;
-        private ITaskRepository _taskRepo;
-        private IMapper mapper;
+        private Mock<ITaskRepository> _taskRepo;
+        private IMapper _mapper;
 
         public TaskControllerTest()
         {
-            _fixture = new Fixture().Customize(new AutoMoqCustomization());
-            _taskRepo = _fixture.Freeze<ITaskRepository>();
+            _fixture = new Fixture();
+            _taskRepo = new Mock<ITaskRepository>();
 
             var mapperProfile = new Maps();
             var configurationMapper = new MapperConfiguration(cfg => cfg.AddProfile(mapperProfile));
-            mapper = new Mapper(configurationMapper);
+            _mapper = new Mapper(configurationMapper);
 
         }
 
@@ -34,16 +36,28 @@
             public void Test_Get_All_Task()
             {
                 //Arrange
-                TasksController controller = new TasksController(_taskRepo, mapper); // Controller
+                var tasks = _fixture.Create<List<Task>>();
+                _taskRepo.Setup(x => x.All()).Returns(tasks);
+                TasksController controller = new TasksController(_taskRepo.Object, _mapper); // Controller
 
                 //Act
+                var mappedTasks = _mapper.Map< List<Task>, List<TaskVM>>(tasks);
                 var actionResult = controller.Get(); // Action to test
+
                 var result = actionResult as OkObjectResult;
+                var resultTasks = result.Value as List<TaskVM>;
 
                 // Assert
                 Assert.NotNull(result);
                 Assert.NotNull(result.Value);
                 Assert.Equal(200, result.StatusCode);
+
+                Assert.All(resultTasks, resultTask => {
+                    Assert.Contains(mappedTasks, task => task.id == resultTask.id);
+                    Assert.Contains(mappedTasks, task => task.description == resultTask.description);
+                });
+
+                //Assert.Equal(mappedTasks, resultTasks);
             }
         }
 
@@ -55,22 +69,39 @@
             {
                 //Arrange
                 var task = _fixture.Create<Task>();
-                TasksController controller = new TasksController(_taskRepo, mapper); // Controller
-                //_taskRepo.Setup();
+                _taskRepo.Setup(x => x.Find(task.id)).Returns(task);
+                TasksController controller = new TasksController(_taskRepo.Object, _mapper); // Controller
 
                 //Act
+                var mappedTask = _mapper.Map<Task, TaskVM>(task);
+                var actionResult = controller.GetById(task.id); // Action to test
+
+                var result = actionResult as OkObjectResult;
+                var resultTask = result.Value as TaskVM;
 
                 // Assert
+                Assert.NotNull(result);
+                Assert.NotNull(resultTask);
+
+                Assert.Equal(200, result.StatusCode);
+                Assert.Equal(mappedTask.id, resultTask.id);
+                //Assert.Equal(mappedTask, resultTask);
             }
 
             [Fact]
             public void Test_Get_A_Task_NotFound()
             {
                 //Arrange
+                _taskRepo.Setup(x => x.Find(It.IsAny<int>())).Returns(null as Task);
+                TasksController controller = new TasksController(_taskRepo.Object, _mapper); // Controller
 
                 //Act
+                var actionResult = controller.GetById(1); // Action to test
+                var result = actionResult as NotFoundResult;
 
-                // Assert   
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal(404, result.StatusCode);
             }
         }
 
